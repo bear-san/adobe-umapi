10645bc25a97a5d7e56beef7e1967c9ff421d6dc
Kentaro Abe <kentaro@beah.jp>
2024-03-30 03:03:47 +0900

    add: ユーザー系のAPIリクエスト

---
+++ pkg/user/request.go
@@ -0,0 +1,18 @@
+package user
+
+type Request struct {
+	User      string    `json:"user"`
+	RequestID string    `json:"requestID"`
+	Do        []Command `json:"do"`
+}
+
+type Command struct {
+	AddAdobeID          *AddAdobeIDRequest          `json:"addAdobeID,omitempty"`
+	CreateEnterpriseID  *CreateEnterpriseIDRequest  `json:"createEnterpriseID,omitempty"`
+	CreateFederatedID   *CreateFederatedIDRequest   `json:"createFederatedID,omitempty"`
+	AddGroup            *AddGroupRequest            `json:"addGroup,omitempty"`
+	Update              *UpdateRequest              `json:"update,omitempty"`
+	RemoveFromGroup     *RemoveFromGroupRequest     `json:"removeFromGroup,omitempty"`
+	RemoveFromAllGroups *RemoveFromAllGroupsRequest `json:"removeFromAllGroups,omitempty"`
+	RemoveFromOrg       *RemoveFromOrgRequest       `json:"removeFromOrg,omitempty"`
+}
