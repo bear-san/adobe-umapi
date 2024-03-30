c22b04dc6a3c840d0d6cad92ed01ad8c5c17cc3b
Kentaro Abe <kentaro@beah.jp>
2024-03-30 15:35:24 +0900

    add: ユーザーのCRUD系APIリクエスト

---
+++ pkg/api/caller_test.go
@@ -0,0 +1,247 @@
+package api
+
+import (
+	"fmt"
+	"github.com/bear-san/adobe-umapi/pkg/auth"
+	"github.com/bear-san/adobe-umapi/pkg/user"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestExec(t *testing.T) {
+	err := auth.Setup(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"))
+	if err != nil {
+		t.Errorf("failed to setup credential: %v", err)
+	}
+
+	federatedIdDomain := os.Getenv("FEDERATED_ID_DOMAIN")
+	enterpriseIdDomain := os.Getenv("ENTERPRISE_ID_DOMAIN")
+
+	t.Run("Create User", func(t *testing.T) {
+		// 作成系のテスト
+		userRequests := []user.Request{
+			// EnterpriseIDの作成
+			{
+				User:       fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+				RequestID:  "Create_Enterprise_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						CreateEnterpriseID: &user.CreateEnterpriseIDRequest{
+							Email:     fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+							Country:   "JP",
+							FirstName: "Test",
+							LastName:  "EnterpriseID",
+							Option:    "updateIfAlreadyExists",
+						},
+					},
+				},
+			},
+			// FederatedIDの作成
+			{
+				User:       "tools-test",
+				RequestID:  "Create_Federated_ID",
+				Domain:     &federatedIdDomain,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						CreateFederatedID: &user.CreateFederatedIDRequest{
+							Email:     fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+							Country:   "JP",
+							FirstName: "Test",
+							LastName:  "FederatedID",
+							Option:    "updateIfAlreadyExists",
+						},
+					},
+				},
+			},
+		}
+
+		result, err := Exec(&userRequests, auth.Credential, os.Getenv("ORG_ID"), os.Getenv("CLIENT_ID"))
+		if err != nil {
+			t.Errorf("failed to create user: %v", err)
+		}
+
+		if result.Completed != 2 {
+			t.Errorf("failed to create user: %v", result)
+		}
+	})
+
+	t.Run("Add Group", func(t *testing.T) {
+		// グループ追加のテスト
+		userRequests := []user.Request{
+			{
+				User:       fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+				RequestID:  "Add_Group_Enterprise_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						AddGroup: &user.AddGroupRequest{
+							Group: []string{"test-group"},
+						},
+					},
+				},
+			},
+			{
+				User:       fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+				RequestID:  "Add_Group_Federated_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						AddGroup: &user.AddGroupRequest{
+							Group: []string{"test-group"},
+						},
+					},
+				},
+			},
+		}
+
+		result, err := Exec(&userRequests, auth.Credential, os.Getenv("ORG_ID"), os.Getenv("CLIENT_ID"))
+		if err != nil {
+			t.Errorf("failed to add group: %v", err)
+		}
+
+		if result.Completed != 2 {
+			t.Errorf("failed to add group: %v", result)
+		}
+	})
+
+	time.Sleep(5 * time.Second)
+
+	t.Run("Update User", func(t *testing.T) {
+		// 更新系のテスト
+		userRequests := []user.Request{
+			{
+				User:       fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+				RequestID:  "Update_Enterprise_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						Update: &user.UpdateRequest{
+							Email:     fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+							FirstName: "Updated",
+							LastName:  "EnterpriseID",
+							UserName:  fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+						},
+					},
+				},
+			},
+			{
+				User:       fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+				RequestID:  "Update_Federated_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						Update: &user.UpdateRequest{
+							Email:     fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+							FirstName: "Updated",
+							LastName:  "FederatedID",
+							UserName:  fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+						},
+					},
+				},
+			},
+		}
+
+		result, err := Exec(&userRequests, auth.Credential, os.Getenv("ORG_ID"), os.Getenv("CLIENT_ID"))
+		if err != nil {
+			t.Errorf("failed to update user: %v", err)
+		}
+
+		if result.Completed != 2 {
+			t.Errorf("failed to update user: %v", result)
+		}
+	})
+
+	time.Sleep(5 * time.Second)
+
+	t.Run("Remove User from Group", func(t *testing.T) {
+		// グループからの削除のテスト
+		userRequests := []user.Request{
+			{
+				User:       fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+				RequestID:  "Remove_Group_Enterprise_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						RemoveFromGroup: &user.RemoveFromGroupRequest{
+							Group: []string{"test-group"},
+						},
+					},
+				},
+			},
+			{
+				User:       fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+				RequestID:  "Remove_Group_Federated_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						RemoveFromGroup: &user.RemoveFromGroupRequest{
+							Group: []string{"test-group"},
+						},
+					},
+				},
+			},
+		}
+
+		result, err := Exec(&userRequests, auth.Credential, os.Getenv("ORG_ID"), os.Getenv("CLIENT_ID"))
+		if err != nil {
+			t.Errorf("failed to remove user from group: %v", err)
+		}
+
+		if result.Completed != 2 {
+			t.Errorf("failed to remove user from group: %v", result)
+		}
+	})
+
+	time.Sleep(5 * time.Second)
+
+	t.Run("Remove User from Org", func(t *testing.T) {
+		// 組織からの削除のテスト
+		userRequests := []user.Request{
+			{
+				User:       fmt.Sprintf("tools-test-enterprise-id@%s", enterpriseIdDomain),
+				RequestID:  "Remove_Org_Enterprise_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						RemoveFromOrg: &user.RemoveFromOrgRequest{
+							DeleteAccount: true,
+						},
+					},
+				},
+			},
+			{
+				User:       fmt.Sprintf("tools-test-federated-id@%s", federatedIdDomain),
+				RequestID:  "Remove_Org_Federated_ID",
+				Domain:     nil,
+				UseAdobeID: false,
+				Do: []user.Command{
+					{
+						RemoveFromOrg: &user.RemoveFromOrgRequest{
+							DeleteAccount: true,
+						},
+					},
+				},
+			},
+		}
+
+		result, err := Exec(&userRequests, auth.Credential, os.Getenv("ORG_ID"), os.Getenv("CLIENT_ID"))
+		if err != nil {
+			t.Errorf("failed to remove user from org: %v", err)
+		}
+
+		if result.Completed != 2 {
+			t.Errorf("failed to remove user from org: %v", result)
+		}
+	})
+}
--- pkg/user/request.go
+++ pkg/user/request.go
@@ -3,18 +3,17 @@ package user
 type Request struct {
 	User       string    `json:"user"`
 	RequestID  string    `json:"requestID"`
-	Domain     string    `json:"domain"`
+	Domain     *string   `json:"domain,omitempty"`
 	UseAdobeID bool      `json:"useAdobeID"`
 	Do         []Command `json:"do"`
 }
 
 type Command struct {
-	AddAdobeID          *AddAdobeIDRequest          `json:"addAdobeID,omitempty"`
-	CreateEnterpriseID  *CreateEnterpriseIDRequest  `json:"createEnterpriseID,omitempty"`
-	CreateFederatedID   *CreateFederatedIDRequest   `json:"createFederatedID,omitempty"`
-	AddGroup            *AddGroupRequest            `json:"addGroup,omitempty"`
-	Update              *UpdateRequest              `json:"update,omitempty"`
-	RemoveFromGroup     *RemoveFromGroupRequest     `json:"removeFromGroup,omitempty"`
-	RemoveFromAllGroups *RemoveFromAllGroupsRequest `json:"removeFromAllGroups,omitempty"`
-	RemoveFromOrg       *RemoveFromOrgRequest       `json:"removeFromOrg,omitempty"`
+	AddAdobeID         *AddAdobeIDRequest         `json:"addAdobeID,omitempty"`
+	CreateEnterpriseID *CreateEnterpriseIDRequest `json:"createEnterpriseID,omitempty"`
+	CreateFederatedID  *CreateFederatedIDRequest  `json:"createFederatedID,omitempty"`
+	AddGroup           *AddGroupRequest           `json:"add,omitempty"`
+	Update             *UpdateRequest             `json:"update,omitempty"`
+	RemoveFromGroup    *RemoveFromGroupRequest    `json:"remove,omitempty"`
+	RemoveFromOrg      *RemoveFromOrgRequest      `json:"removeFromOrg,omitempty"`
 }
